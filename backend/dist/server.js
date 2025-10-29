f2ff5ea95c6607f862e4ea5e53d7cc69017b9616
jpg157 <jasonlau5000@gmail.com>
2025-10-29 12:22:26 -0700

    Initial backend api project setup

---
+++ backend/dist/server.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("hello world");
+const test = "hello";
+console.log(test);
+//# sourceMappingURL=server.js.map
